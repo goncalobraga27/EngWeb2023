1559864b0a93d851890fb28eeb7b75aeeb126474
goncalobraga27 <a97541@alunos.uminho.pt>
2023-02-19 16:44:25 +0000

    Ligações da aula prática 1

--- Aulas Praticas/Aula1/geraHTML.py
+++ Aulas Praticas/Aula1/geraHTML.py
@@ -7,6 +7,7 @@ def ordCidade(c):
 f = open("mapa.json")
 mapa = json.load(f)
 cidades = mapa['cidades']
+ligacoes=mapa['ligações']
 
 cidades.sort(key = ordCidade)
 
@@ -48,11 +49,20 @@ for c in cidades:
                         <p><b>População</b>{c['população']}</p>
                         <p><b>Descrição</b>{c['descrição']}</p>
                         <p><b>Distrito</b>{c['distrito']}</p>
+    """
+for c in cidades:
+    for l in ligacoes:
+        if l['origem'] == c['id']:
+            pagHTML+= f"""
+                        <a name="{c['id']}"/>
+                        <h3>Ligações</h3>
+                        <p><b>Destino</b>{l['destino']}</p>
+                        <p><b>Distância</b>{l['distância']}</p>
                         <address><address/>
                         <center>
                             <hr width="80%"/>
                         </center>
-    """
+            """
 
 pagHTML += """
                 </td>
