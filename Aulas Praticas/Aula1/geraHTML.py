16d257c7d9e39e986b1294d0344978e9bf0a1857
goncalobraga27 <a97541@alunos.uminho.pt>
2023-02-19 00:26:24 +0000

    Criação das pastas e dos manifestos

---
+++ Aulas Praticas/Aula1/geraHTML.py
@@ -0,0 +1,65 @@
+import json
+
+# c é uma cidade que é um dicionário
+def ordCidade(c):
+    return c['nome'] # A lista de dicionários (cidades) é ordenada pelo nome
+
+f = open("mapa.json")
+mapa = json.load(f)
+cidades = mapa['cidades']
+
+cidades.sort(key = ordCidade)
+
+pagHTML = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Mapa Virtual</title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        <center>
+            <h1>Mapa Virtual</h1>
+        </center>
+        <table>
+            <tr>
+                <!-- Índice-->
+                <td>
+                    <h3>Índice</h3>
+                    <ul>
+"""
+pagHTML += """
+                    </ul>
+                </td>
+                <!-- Conteúdo-->
+                <td>
+"""
+
+for c in cidades:
+    pagHTML += f"""
+    <li>
+        <a href="{c['id']}">{c['nome']}</a>
+    </li>
+    """
+    pagHTML += f"""
+                        <a name="{c['id']}"/>
+                        <h3>{c['nome']}</h3>
+                        <p><b>População</b>{c['população']}</p>
+                        <p><b>Descrição</b>{c['descrição']}</p>
+                        <p><b>Distrito</b>{c['distrito']}</p>
+                        <address><address/>
+                        <center>
+                            <hr width="80%"/>
+                        </center>
+    """
+
+pagHTML += """
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
+"""
+
+print(pagHTML)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# EngWeb2023
+# TPC's de Engenharia Web 
+# Licenciatura em Engenharia Informática(3ºAno-2ºsem)
+# Autor-Gonçalo Semelhe Sousa Braga (A97541)
